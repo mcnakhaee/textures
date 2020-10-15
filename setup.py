f1dcf14c585411ddd2ae01f3fba173645ecb348f
mcnakhaee <mcnakhaee@gmail.org>
2020-10-15 14:43:58 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 import setuptools
 setup(name='textures',
-      version='0.1.5',
+      version='0.1.6',
       url="https://github.com/mcnakhaee/textures",
       description="A python package to extract features from text data",
       long_description=open('DESCRIPTION.rst').read(),
       author='Muhammad Chenariyan Nakhaee',
       author_emai='mcnakhaee@gmail.com',
       packages = ['textures'],
-      install_requires=['pandas', 'spacy','pyenchant','textblob','nltk','langdetect','textstat','pyenchant'],
+      install_requires=['pandas', 'spacy','pyenchant','textblob','nltk','langdetect','textstat','pyenchant','pytest'],
       include_package_data=True,
       package_data={'': ['data/*.csv']},
       )
